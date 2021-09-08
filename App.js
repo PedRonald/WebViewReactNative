b38b6a444c3f7c7f38e2ef0dcaba736c5844e08a
PedRonald <pronalmsvg@gmail.com>
2021-09-08 17:59:07 -0300

    first commit

--- App.js
+++ App.js
@@ -1,11 +1,16 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <WebView 
+      style={styles.container}
+      source={{ uri: 'https://ulb.org.br/' }}
+    />
+      
       <StatusBar style="auto" />
     </View>
   );
@@ -14,8 +19,5 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });

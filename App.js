a56f1a5f220f0e9df641d1276d5cbb338c8a99ff
Murilo Teixeira <murilogteixeira@gmail.com>
2021-10-08 12:08:39 -0300

    Fix safeArea spacing

--- App.js
+++ App.js
@@ -2,17 +2,18 @@ import { StatusBar } from 'expo-status-bar';
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { WebView } from 'react-native-webview';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container} edges={['top', 'left', 'right']}>
       <WebView 
       style={styles.container}
       source={{ uri: 'https://ulb.org.br/' }}
     />
       
       <StatusBar style="auto" />
-    </View>
+    </SafeAreaView>
   );
 }
 
